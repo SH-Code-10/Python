d90a8a0ca4e3d3984db01e34c028c7d8ea9ef365
Sh-venom <shishir.shx10@gmail.com>
2026-01-02 11:44:31 +0545

    Learing python

---
+++ learning.py
@@ -0,0 +1,40 @@
+
+##PRACTISING TO CONNECT NUM WITH STRING
+# num=10;
+# message ="my favourite number is "+ str(num) +"." //str(num)convert the number to string
+# print(message)
+
+
+## USING THE BASIC STRING METHOD 
+# name=" shishir ";
+# print(name.title());
+# // make the first letter of name capital
+# print(name.upper());
+# //make all the letter of name to capital
+# print(name.lower());
+# //make lower
+# print(name.strip())
+# //removing the unnecessary whitespace in the variable name
+
+##BASIC EXAMPLE FOR LIST IN PYTHON
+# list=["hello","welcome","to","my","learning","platform"];
+# print(list[0].upper()+","+list[1].title()+list[2]+list[3]+list[4]+list[5])
+# //OUTPUT:
+# //HELLO,Welcometomylearningplatform
+
+# #LISTING METHOD
+# invited_person=["aayush","reman","sujan","barun"]
+# invited_person.append("samyak") 
+# //append add samyak to the last of the list
+# print(invited_person[4].title()+" is the new person who is added to party")
+# invited_person[1]="ME" 
+#  //position 1 of the list reman is replaced by ME
+# print(invited_person[1].title()+" is the replacable person")
+# not_comming=invited_person.pop()
+# print(not_comming.title()+" is not coming cuz late invited")
+# //OUTPUT:
+# Samyak is the new person who is added to party
+# Me is the replacable person
+# Samyak is not coming cuz late invited
+
+
